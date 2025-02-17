4c42dc80f8597a9a8f8bb95c54e65662c7f59c3b
zfdx123 <2915441170@qq.com>
2025-02-17 22:45:07 +0800

    test

--- luci-app-vlmcsd/htdocs/luci-static/resources/view/vlmcsd.js
+++ luci-app-vlmcsd/htdocs/luci-static/resources/view/vlmcsd.js
@@ -12,6 +12,20 @@ const callServiceList = rpc.declare({
     expect: { '': {} }
 });
 
+const callServiceStart = rpc.declare({
+    object: 'service',
+    method: 'start',
+    params: ['name'],
+    expect: { '': {} }
+});
+
+const callServiceStop = rpc.declare({
+    object: 'service',
+    method: 'stop',
+    params: ['name'],
+    expect: { '': {} }
+});
+
 function getServiceStatus() {
     return L.resolveDefault(callServiceList('vlmcsd'), {}).then(res =>
         res?.['vlmcsd']?.['instances']?.['vlmcsd']?.['running']
@@ -25,6 +39,17 @@ function renderStatus(status) {
     return `<em><span style="color:${color}"><strong>${service} ${running}</strong></span></em>`;
 }
 
+function toggleService(action) {
+    return (status) => {
+        const service = 'vlmcsd';
+        if (action === 'start' && !status) {
+            return callServiceStart(service).then(() => _('Starting Vlmcsd KMS Server...'));
+        } else if (action === 'stop' && status) {
+            return callServiceStop(service).then(() => _('Stopping Vlmcsd KMS Server...'));
+        }
+    };
+}
+
 return view.extend({
     render: function () {
         const m = new form.Map('vlmcsd', _('Vlmcsd KMS Server'));
@@ -34,14 +59,22 @@ return view.extend({
         s.render = function () {
             poll.add(function () {
                 return L.resolveDefault(getServiceStatus()).then(function (res) {
-                    const stats = renderStatus(res);
+                    const status = res;
+                    const stats = renderStatus(status);
                     const view = document.getElementById('vlmcsd_status');
                     view.innerHTML = stats;
                 });
             });
 
             return E('div', { class: 'cbi-section', id: 'status_bar' }, [
-                E('p', { id: 'vlmcsd_status' }, _('Collecting data…'))
+                E('p', { id: 'vlmcsd_status' }, _('Collecting data…')),
+                E('button', {
+                    onclick: function () {
+                        L.resolveDefault(getServiceStatus()).then(function (status) {
+                            toggleService(status ? 'stop' : 'start')(status);
+                        });
+                    }
+                }, _('Toggle Service'))
             ]);
         };
 
@@ -49,8 +82,13 @@ return view.extend({
         s.tab('general', _('General Settings'));
         s.tab('config_file', _('Configuration File'), _('Edit the content of the /etc/vlmcsd/vlmcsd.ini file.'));
 
-        s.taboption('general', form.Flag, 'enabled', _('Enable Vlmcsd KMS Server'));
-        s.taboption('general', form.Flag, 'auto_activate', _('Allow automatic activation'));
+        s.taboption('general', form.Flag, 'enabled', _('Enable Vlmcsd KMS Server'), {
+            default: true
+        });
+        s.taboption('general', form.Value, 'port', _('KMS Server Port'), {
+            default: 1688,
+            datatype: "number"
+        }).placeholder = '1688';
 
         const o = s.taboption('config_file', form.TextValue, '_tmpl',
             null,
